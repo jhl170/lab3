54f30d713e262b550d05a0c631d0eb4ed88fc8b7
Jun Lee <jhl170@ucsd.edu>
2017-01-26 01:56:40 -0800

    Finished up to Heroku

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,29 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
+	$("a.thumbnail").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+	// prevent page from reloading
+	e.preventDefault();
+
+	// In an event handler, $(this) refers to
+	// the object that triggered the event
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(".project-description").fadeOut();
+       $(".project-description").fadeIn();
+       $(".project-description").fadeOut();
+    }
 }
